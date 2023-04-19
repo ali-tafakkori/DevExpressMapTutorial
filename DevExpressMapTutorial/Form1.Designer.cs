59efcc646f365ac01c1efb88f2b2bbf910330f64
Ali Tafakkori <alitafakkori82@gmail.com>
2023-04-20 00:23:13 +0330

    Update Form1.Designer.cs

--- DevExpressMapTutorial/Form1.Designer.cs
+++ DevExpressMapTutorial/Form1.Designer.cs
@@ -29,13 +29,41 @@ namespace DevExpressMapTutorial
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mapControl1 = new DevExpress.XtraMap.MapControl();
+            this.openStreetMapDataProvider1 = new DevExpress.XtraMap.OpenStreetMapDataProvider();
+            this.imageLayer1 = new DevExpress.XtraMap.ImageLayer();
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mapControl1
+            // 
+            this.mapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapControl1.Layers.Add(this.imageLayer1);
+            this.mapControl1.Location = new System.Drawing.Point(0, 0);
+            this.mapControl1.Name = "mapControl1";
+            this.mapControl1.Size = new System.Drawing.Size(800, 450);
+            this.mapControl1.TabIndex = 0;
+            this.openStreetMapDataProvider1.TileUriTemplate = "https://{0}.tile.INSERT_SERVER_NAME.com/{1}/{2}/{3}.png";
+            this.imageLayer1.DataProvider = this.openStreetMapDataProvider1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mapControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DevExpress.XtraMap.MapControl mapControl1;
+        private DevExpress.XtraMap.ImageLayer imageLayer1;
+        private DevExpress.XtraMap.OpenStreetMapDataProvider openStreetMapDataProvider1;
     }
 }
 
